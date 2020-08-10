64c1bf62ae394c6016c881a20a04c4ac6d1b84b1
your name <tubameral87@hotmail.com>
2020-08-09 20:44:21 -0400

    the second commit from local repo

--- src/package2/firstclass.java
+++ src/package2/firstclass.java
@@ -5,6 +5,8 @@ public class firstclass {
 	public static void main(String[] args) {
 		
 		System.out.println("this is the first changes from local master");
+		System.out.println("this is the second commit from local master");
+		
 	}
 
 }
