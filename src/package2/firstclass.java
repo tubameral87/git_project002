fd4ae64714938184f3c8259861cbea6468bde034
tubameral87 <69390525+tubameral87@users.noreply.github.com>
2020-08-09 21:34:15 -0400

    Update firstclass.java
    this is the first commit from the remote master

--- src/package2/firstclass.java
+++ src/package2/firstclass.java
@@ -7,6 +7,7 @@ public class firstclass {
 		System.out.println("this is the first changes from local master");
 		System.out.println("this is the second commit from local master");
 		System.out.println("this is the third commit from local master");
+		System.out.println("this is the first commit from remote repo");
 	}
 
 }
